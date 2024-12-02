a2cafe3cd172a19f42c69c0be16c335354d8d92f
hshahz <haroon@shahzadonline.com>
2024-12-01 21:10:42 -0600

    Added new reqs, code functionalitya dded

--- README.md
+++ README.md
@@ -3,7 +3,13 @@ discord bot that runs the haroon bot account
 
 # HOW TO RUN
 - Pull repo locally
+
+## specify discord api key
 - create ".env" dir locally in root folder haroonbot
 - inside of .env, create file named env.py
-- in env.py, label two variables "api_key" and "aws_key" for connecting to discord api and aws boto3 dynamoDB
-- put keys in env file
+- in env.py, label variable "api_key" for connecting to discord api
+- put key in env file
+
+## specify aws key
+- in C:/Users/<you>/.aws/credentials, add aws access key and private key
+- in C:/Users/<you>/.aws/config, set region to us-east-2
--- src/main.py
+++ src/main.py
@@ -1,16 +1,31 @@
 import discord
-from
+import boto3
+from env.env import discord_api_key, table_name
+
+
+
 
 
 class HaroonBot(discord.Client):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #Init DynamoDB
+        self.dynamodb = boto3.resource('dynamodb')
+        self.table = self.dynamodb.Table(table_name)
+
     async def on_ready(self):
         print(f'Logged on as {self.user}')
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
         if message.content.contains("!geo"):
-        #scrape for <score>
-        #if no score then say hey ur score is not a valid format
+            try: #scrape for <score>
+                score = int(message.content.split("!geo")[1].strip())
+                await message.channel.send(f"Your score today was {score}")
+            except(IndexError, ValueError): #if no score then say hey ur score is not a valid format
+                await message.channel.send("Invalid format. Use `!geo <score> with a numerical only score")
+                return
 
         #DETECT SOME SOR OF INPUT LIKE !geo <your_score>
         # message.author.userid
@@ -24,4 +39,5 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = HaroonBot(intents=intents)
-client.run(api_key)
+client.run(discord_api_key)
+
