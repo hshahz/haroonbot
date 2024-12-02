2075143fc324c90c82db3fff02859e7217ef670f
hshahz <haroon@shahzadonline.com>
2024-12-01 21:37:23 -0600

    it gets online

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import discord
 import boto3
-from env.env import discord_api_key, table_name
+from env.env import get_discord_api_key, get_table_name
 
 
 
@@ -11,15 +11,18 @@ class HaroonBot(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         #Init DynamoDB
-        self.dynamodb = boto3.resource('dynamodb')
-        self.table = self.dynamodb.Table(table_name)
+        self.dynamodb = boto3.resource('dynamodb', region_name="us-east-2")
+        self.table = self.dynamodb.Table(get_table_name())
 
     async def on_ready(self):
         print(f'Logged on as {self.user}')
 
     async def on_message(self, message):
+        if message.author == self.user: #prevent loop
+            return
+
         print(f'Message from {message.author}: {message.content}')
-        if message.content.contains("!geo"):
+        if "!geo" in message.content: #DETECT SOME SOR OF INPUT LIKE !geo <your_score>
             try: #scrape for <score>
                 score = int(message.content.split("!geo")[1].strip())
                 await message.channel.send(f"Your score today was {score}")
@@ -27,7 +30,8 @@ class HaroonBot(discord.Client):
                 await message.channel.send("Invalid format. Use `!geo <score> with a numerical only score")
                 return
 
-        #DETECT SOME SOR OF INPUT LIKE !geo <your_score>
+
+        #leaderboard format:
         # message.author.userid
         # user1, user2, user3, user4, etc.
         # day 1, 10k, 15k , 14k,
@@ -39,5 +43,5 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = HaroonBot(intents=intents)
-client.run(discord_api_key)
+client.run(get_discord_api_key())
 
